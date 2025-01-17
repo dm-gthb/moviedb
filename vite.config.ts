f19b0389d42744fcddeebceb8c8a33d003db393b
Dmitry Mamonov <indexdm@yandex.ru>
2025-01-17 15:56:07 +0400

    setup tests

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
 
-// https://vite.dev/config/
+import react from '@vitejs/plugin-react';
+import { defineConfig, type UserConfig } from 'vite';
+
 export default defineConfig({
   plugins: [react()],
-});
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['./src/setupTests.ts'],
+  },
+} as UserConfig);

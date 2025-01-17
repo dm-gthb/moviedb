243aadd5dd95c6bad1f9e9af6320ad9772c07967
Dmitry Mamonov <indexdm@yandex.ru>
2025-01-17 16:04:29 +0400

    remove unused

--- src/mocks/server.ts
+++
@@ -1,7 +0,0 @@
-import { setupWorker } from 'msw/browser';
-import { user } from './handlers/user';
-import { movies } from './handlers/movies';
-import { listItems } from './handlers/list-items';
-import { rating } from './handlers/rating';
-
-export const worker = setupWorker(...user, ...movies, ...listItems, ...rating);

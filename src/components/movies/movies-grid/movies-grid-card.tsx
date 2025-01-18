9683199e21ae9acf97bc8d83b0240448f62b6cd9
Dmitry Mamonov <indexdm@yandex.ru>
2025-01-18 16:25:34 +0400

    add movies grid styles

--- src/components/movies/movies-grid/movies-grid-card.tsx
+++ src/components/movies/movies-grid/movies-grid-card.tsx
@@ -2,21 +2,50 @@ import { Link, useLocation } from 'react-router';
 import { MovieItem } from '../../../services/movies/movies.types.service';
 import { useSearchParamsWithMoviesFilterDefaults } from '../../../services/movies/movies-filter.service';
 import { ListItemButtons } from '../list-item-buttons/list-item-buttons';
+import { genresMap } from '../../../services/movies/movies.constants.service';
+import { MoviePoster } from '../movie-poster/movie-poster';
 
-export function MoviesGridCard({ movie }: { movie: MovieItem }) {
+export function MovieCard({ movie }: { movie: MovieItem }) {
   const searchParams = useSearchParamsWithMoviesFilterDefaults();
   const { pathname } = useLocation();
-  const { id, title } = movie;
+  const { id, title, posterPath } = movie;
 
   return (
-    <div>
+    <article className="">
       <Link
         to={`/movie/${id}`}
         state={{ searchParams: searchParams.toString(), pathname }}
       >
-        <h2>{title}</h2>
+        <h2 className="sr-only">{title}</h2>
+        <div className="transition group hover:scale-110 rounded relative shadow-md overflow-hidden">
+          <MoviePoster posterPath={posterPath} movieTitle={title} />
+          <div className="px-2 py-3 text-white dark:text-gray-200 bg-gray-700 dark:bg-gray-800 rounded-b opacity-0 group-hover:opacity-100 transition absolute bottom-0 left-0 w-full">
+            <div className="flex h-full items-center gap-2 text-sm mb-2">
+              {movie.voteAverage > 0 && (
+                <div
+                  className={`${movie.voteAverage >= 7 ? 'bg-green-600 dark:bg-green-600/80' : 'bg-gray-500'} px-1 rounded text-white`}
+                >
+                  <span>{movie.voteAverage?.toFixed(1)}</span>
+                </div>
+              )}
+              <span>{movie.releaseDate.slice(0, 4)}</span>
+              <span className="overflow-ellipsis">
+                {getFirstGenreName(movie.genreIds)}
+              </span>
+            </div>
+            <ListItemButtons movie={movie} />
+          </div>
+        </div>
       </Link>
-      <ListItemButtons movie={movie} />
-    </div>
+    </article>
   );
+}
+
+export function PlaceholderMovieCard() {
+  return <MoviePoster />;
+}
+
+function getFirstGenreName(genreIds: number[]) {
+  const genreNames = genreIds?.map((id) => genresMap[id]);
+  return genreNames[0];
 }
--- src/components/movies/movies-grid/movies-grid.tsx
+++ src/components/movies/movies-grid/movies-grid.tsx
@@ -1,14 +1,18 @@
 import { MovieItem } from '../../../services/movies/movies.types.service';
-import { MoviesGridCard } from './movies-grid-card';
+import { MovieCard, PlaceholderMovieCard } from './movies-grid-card';
 
-export function MoviesGrid({ movies }: { movies: MovieItem[] }) {
+export function MoviesGrid({
+  isPending,
+  movies,
+}: {
+  isPending?: boolean;
+  movies?: MovieItem[];
+}) {
   return (
-    <ol>
-      {movies.map((movie) => (
-        <li key={movie.id}>
-          <MoviesGridCard movie={movie} />
-        </li>
-      ))}
-    </ol>
+    <div className="grid grid-cols-2 gap-6 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 lg:gap-8 xl:gap-10">
+      {isPending
+        ? new Array(20).fill('').map((_el, i) => <PlaceholderMovieCard key={i} />)
+        : movies?.map((movie) => <MovieCard key={movie.id} movie={movie} />)}
+    </div>
   );
 }

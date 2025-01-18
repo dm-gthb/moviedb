740c5484e2fb185b67dcbba5b6f77cf20c227093
Dmitry Mamonov <indexdm@yandex.ru>
2025-01-18 17:00:38 +0400

    update search page

--- src/pages/search/search.page.tsx
+++ src/pages/search/search.page.tsx
@@ -10,13 +10,25 @@ export function SearchPage() {
     <div className="max-w-7xl mx-auto px-8 pt-4 pb-10">
       <h1 className="font-bold text-4xl md:text-5xl mb-5">Search Results</h1>
       <p className="text-xl max-w-2xl mb-6">
-        Searching for: <span className='italic capitalize font-semibold'>"{searchTerm}"</span>
+        {isPending && (
+          <>
+            Searching for: <SearchTerm term={searchTerm} />
+          </>
+        )}
+        {!isPending && !data?.results?.length && (
+          <>
+            No movies found for <SearchTerm term={searchTerm} />.
+          </>
+        )}
       </p>
 
       <div className="mb-10">
         <MoviesGrid movies={data?.results} isPending={isPending} />
-        {!isPending && !data?.results.length && <span>{`No movies found for term ${searchTerm}.`}</span>}
       </div>
     </div>
   );
+}
+
+function SearchTerm({ term = '' }: { term?: string }) {
+  return <span className="italic capitalize font-semibold">"{term}"</span>;
 }

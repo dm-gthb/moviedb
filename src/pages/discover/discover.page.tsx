ca22acd3ebbcc0fa873688abbb075b2bb0c4335e
Dmitry Mamonov <indexdm@yandex.ru>
2025-01-18 16:26:03 +0400

    add discover page styles

--- src/pages/discover/discover.page.tsx
+++ src/pages/discover/discover.page.tsx
@@ -7,31 +7,32 @@ export function DiscoverPage() {
     useFilteredMovies();
   const movies = data?.pages.map(({ results }) => results).flat() ?? [];
 
-  if (isPending) {
-    return (
-      <>
-        <h1>DiscoverPage</h1>
-        <span>...loading</span>
-      </>
-    );
-  }
-
   return (
-    <>
-      <h1>DiscoverPage</h1>
-      <MoviesFilter />
+    <div className="max-w-7xl mx-auto px-8 pt-4 pb-10">
+      <h1 className="font-bold text-4xl md:text-5xl mb-5">Discover Movies</h1>
+      <p className="text-xl max-w-2xl mb-6">
+        Explore a vast collection of films, from the latest releases to timeless classics,
+        with data powered by The Movie Database (TMDB).
+      </p>
 
-      {movies.length > 0 ? (
-        <MoviesGrid movies={movies} />
-      ) : (
-        <span>no such movies found</span>
-      )}
+      <div className="mb-10">
+        <MoviesFilter />
+      </div>
+
+      <div className="mb-10">
+        <MoviesGrid movies={movies} isPending={isPending} />
+        {!isPending && !movies.length && <span>No such movies found</span>}
+      </div>
 
       {hasNextPage && (
-        <button disabled={isFetchingNextPage} onClick={() => fetchNextPage()}>
-          load more
+        <button
+          disabled={isFetchingNextPage}
+          onClick={() => fetchNextPage()}
+          className="w-full p-4 text-center bg-gray-200 dark:bg-gray-700 hover:bg-gray-300 hover:dark:bg-gray-800 disabled:text-gray-400 transition-colors rounded"
+        >
+          Load More
         </button>
       )}
-    </>
+    </div>
   );
 }

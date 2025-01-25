08f063abea0fb37d434c45cdc57dded0ce4c4d84
Dmitry Mamonov <indexdm@yandex.ru>
2025-01-25 20:26:20 +0400

    update movie card to improve keyboard navigation

--- src/components/movies/movies-grid/movies-grid-card.tsx
+++ src/components/movies/movies-grid/movies-grid-card.tsx
@@ -15,11 +15,12 @@ export function MovieCard({ movie }: { movie: MovieItem | ListItemMovie }) {
       <Link
         to={`/movie/${id}`}
         state={{ searchParams: searchParams.toString(), pathname }}
+        className="group"
       >
         <h2 className="sr-only">{title}</h2>
-        <div className="transition group hover:scale-110 rounded relative shadow-md overflow-hidden">
+        <div className="relative group-hover:scale-110 group-focus-within:scale-110 transition rounded overflow-hidden shadow-md">
           <MoviePoster posterPath={posterPath} movieTitle={title} />
-          <div className="px-2 py-3 text-white dark:text-gray-200 bg-gray-700 dark:bg-gray-800 rounded-b opacity-0 group-hover:opacity-100 transition absolute bottom-0 left-0 w-full">
+          <div className="absolute bottom-0 left-0 w-full px-2 py-3 text-white dark:text-gray-200 bg-gray-700 dark:bg-gray-800 rounded-b opacity-0 group-hover:opacity-100 group-focus-within:opacity-100 transition-opacity">
             <div className="flex h-full items-center gap-2 text-sm mb-2">
               <span>{releaseDate?.slice(0, 4)}</span>
               <span className="overflow-ellipsis">{getFirstGenreName(genreIds)}</span>

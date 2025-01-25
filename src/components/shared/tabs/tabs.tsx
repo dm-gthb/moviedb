d7d4fc54beabb1560eed40870cfb69fc380840af
Dmitry Mamonov <indexdm@yandex.ru>
2025-01-25 20:31:27 +0400

    rewrite tabs component, add keyboard navigation

--- src/components/shared/tabs/tabs.tsx
+++ src/components/shared/tabs/tabs.tsx
@@ -1,48 +1,126 @@
 import {
+  Children,
+  cloneElement,
   createContext,
   Dispatch,
+  ReactElement,
   ReactNode,
   SetStateAction,
   useContext,
   useState,
+  KeyboardEvent,
+  useRef,
+  RefCallback,
 } from 'react';
 
-const TabsContext = createContext<{
-  activeTab: string;
-  setActiveTab: Dispatch<SetStateAction<string>>;
-}>({
-  activeTab: '',
-  setActiveTab: () => {},
-});
+const TabsContext = createContext<
+  | { activeTabIndex: number; setActiveTabIndex: Dispatch<SetStateAction<number>> }
+  | undefined
+>(undefined);
+
+function useTabs() {
+  const context = useContext(TabsContext);
+  if (context === undefined) {
+    throw new Error(`useTabs must be used within TabsContext provider`);
+  }
+  return context;
+}
 
 export function Tabs({
   children,
-  defaultValue,
+  defaultIndex,
 }: {
   children: ReactNode;
-  defaultValue?: string;
+  defaultIndex?: number;
 }) {
-  const [activeTab, setActiveTab] = useState(defaultValue ?? '');
+  const [activeTabIndex, setActiveTabIndex] = useState(defaultIndex ?? 0);
   return (
-    <TabsContext.Provider value={{ activeTab, setActiveTab }}>
+    <TabsContext.Provider value={{ activeTabIndex, setActiveTabIndex }}>
       {children}
     </TabsContext.Provider>
   );
 }
 
-export function TabTrigger({
-  value,
-  label,
+export function TabList({
+  children,
   className,
-  onClick,
 }: {
-  value: string;
-  label: string;
+  children: ReactElement<TabTriggerProps>[];
+  className?: string;
+}) {
+  const { setActiveTabIndex } = useTabs();
+  const tabsCount = children.length;
+  const itemsRef = useRef<Map<number, HTMLButtonElement> | null>(null);
+
+  const getMap = () => {
+    if (!itemsRef?.current) {
+      itemsRef.current = new Map();
+    }
+    return itemsRef.current;
+  };
+
+  const handleKeydown = (e: KeyboardEvent, index: number) => {
+    const activateTab = (index: number) => {
+      setActiveTabIndex(index);
+      const map = getMap();
+      const node = map.get(index);
+      node?.focus();
+    };
+
+    if (e.key === 'ArrowRight') {
+      if (index + 1 < tabsCount) {
+        return activateTab(index + 1);
+      }
+
+      return activateTab(0);
+    }
+
+    if (e.key === 'ArrowLeft') {
+      if (index > 0) {
+        return activateTab(index - 1);
+      }
+      return activateTab(tabsCount - 1);
+    }
+  };
+
+  return (
+    <div role="tablist" className={className}>
+      {Children.map(children, (child, index) => {
+        return cloneElement(child, {
+          ...child.props,
+          index,
+          handleKeydown,
+          ref: (node: HTMLButtonElement) => {
+            const map = getMap();
+            map.set(index, node);
+            return () => {
+              map.delete(index);
+            };
+          },
+        });
+      })}
+    </div>
+  );
+}
+
+type TabTriggerProps = {
+  children: string;
   className?: string;
   onClick?: (args: unknown) => void;
-}) {
-  const { activeTab, setActiveTab } = useContext(TabsContext);
-  const isActive = activeTab === value;
+  index?: number;
+  handleKeydown?: (e: KeyboardEvent<HTMLButtonElement>, index: number) => void;
+  ref?: RefCallback<HTMLButtonElement>;
+};
+export function TabTrigger({
+  children: label,
+  className,
+  onClick,
+  index = 0,
+  handleKeydown,
+  ref,
+}: TabTriggerProps) {
+  const { activeTabIndex, setActiveTabIndex } = useTabs();
+  const isActive = activeTabIndex === index;
 
   return (
     <button
@@ -50,15 +128,46 @@ export function TabTrigger({
       className={className}
       onClick={(...args) => {
         onClick?.(...args);
-        setActiveTab(value);
+        setActiveTabIndex(index);
       }}
+      role="tab"
+      aria-selected={isActive}
+      aria-controls={`tabPanel-${index}`}
+      tabIndex={isActive ? 0 : -1}
+      onKeyDown={(e) => handleKeydown?.(e, index)}
+      ref={ref}
     >
       {label}
     </button>
   );
 }
 
-export function TabContent({ value, children }: { value: string; children: ReactNode }) {
-  const { activeTab } = useContext(TabsContext);
-  return value === activeTab ? children : null;
+export function TabPanels({
+  children,
+  className,
+}: {
+  children: ReactElement<TabPanelProps>[];
+  className?: string;
+}) {
+  return (
+    <div className={className}>
+      {Children.map(children, (child, index) => {
+        return cloneElement(child, { ...child.props, index });
+      })}
+    </div>
+  );
+}
+
+type TabPanelProps = {
+  children: ReactNode;
+  index?: number;
+};
+export function TabPanel({ children, index }: TabPanelProps) {
+  const { activeTabIndex } = useTabs();
+
+  return activeTabIndex === index ? (
+    <div role="tabpanel" id={`tabPanel-${index}`}>
+      {children}
+    </div>
+  ) : null;
 }

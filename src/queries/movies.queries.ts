f4a8046f855f4f4e62325398a9c1b51def39dabc
Dmitry Mamonov <indexdm@yandex.ru>
2025-01-27 18:54:56 +0400

    remove unused

--- src/queries/movies.queries.ts
+++ src/queries/movies.queries.ts
@@ -88,7 +88,6 @@ export const useMovieDetails = ({ movieId }: { movieId: string }) => {
           }),
       },
       movieQueries.credits(movieId),
-      movieQueries.recommendations(movieId),
     ],
   });
 };

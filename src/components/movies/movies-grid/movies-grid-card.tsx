5c2e30caa27870520005d56e8e9d0277d2905474
Dmitrii Mamonov <dm.ggl2022@gmail.com>
2025-03-29 18:02:56 +0400

    update movie card styles

--- src/components/movies/movies-grid/movies-grid-card.tsx
+++ src/components/movies/movies-grid/movies-grid-card.tsx
@@ -15,10 +15,10 @@ export function MovieCard({ movie }: { movie: MovieItem | ListItemMovie }) {
       <Link
         to={`/movie/${id}`}
         state={{ searchParams: searchParams.toString(), pathname }}
-        className="group"
+        className="group block overflow-hidden rounded shadow-md transition focus-within:scale-110 hover:scale-110"
       >
         <h2 className="sr-only">{title}</h2>
-        <div className="relative overflow-hidden rounded shadow-md transition group-focus-within:scale-110 group-hover:scale-110">
+        <div className="relative">
           <MoviePoster posterPath={posterPath} movieTitle={title} />
           <div className="absolute bottom-0 left-0 w-full rounded-b bg-gray-700 px-2 py-3 text-white opacity-0 transition-opacity group-focus-within:opacity-100 group-hover:opacity-100 dark:bg-gray-800 dark:text-gray-200">
             <div className="mb-2 flex h-full items-center gap-2 text-sm">

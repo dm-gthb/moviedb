54db03b6950006bb50db0e35e6fd770a3d4da8b3
Dmitry Mamonov <dm.ggl2022@gmail.com>
2025-08-09 15:38:47 +0400

    update AuthForm

--- src/components/auth/auth-form/auth-form.tsx
+++ src/components/auth/auth-form/auth-form.tsx
@@ -3,20 +3,29 @@ import { AuthData } from '../../../services/auth/auth.types.service';
 import { getErrorMessage } from '../../../services/utils.service';
 import { FormField } from '../../ui/forms/form-field/form-field';
 import { PasswordVisibilityToggler } from './password-visibility-toggler';
-import { SubmitButton } from './submit-button';
+import { AuthFormError } from './auth-form-error';
+import { Spinner } from '../../ui/spinner/spinner';
 
 const PASSWORD_MIN_LENGTH = 8;
-
-export type AuthFormProps = {
-  type: 'login' | 'signup';
-  onSubmit: (data: AuthData) => Promise<void>;
-};
 
 type FormState = {
   data: AuthData;
   isError: boolean;
   errorMessage: string;
 };
+
+type AuthFormProps = {
+  type: 'login' | 'signup';
+  onSubmit: (data: AuthData) => Promise<void>;
+};
+
+export function LoginForm({ onSubmit }: { onSubmit: AuthFormProps['onSubmit'] }) {
+  return <AuthForm type="login" onSubmit={onSubmit} />;
+}
+
+export function SignupForm({ onSubmit }: { onSubmit: AuthFormProps['onSubmit'] }) {
+  return <AuthForm type="signup" onSubmit={onSubmit} />;
+}
 
 function AuthForm(props: AuthFormProps) {
   const { type, onSubmit } = props;
@@ -39,8 +48,6 @@ function AuthForm(props: AuthFormProps) {
     { data: { email: '', password: '' }, isError: false, errorMessage: '' },
   );
 
-  const formErrors = formState?.isError ? [formState.errorMessage] : undefined;
-
   return (
     <form action={formAction}>
       <fieldset disabled={isPending}>
@@ -54,7 +61,6 @@ function AuthForm(props: AuthFormProps) {
             autoFocus: true,
             required: true,
           }}
-          errors={formErrors}
         />
         <FormField
           labelProps={{ children: 'Password' }}
@@ -76,15 +82,24 @@ function AuthForm(props: AuthFormProps) {
           }
         />
       </fieldset>
-      <SubmitButton type={type} isDisabled={isPending} />
+      {formState?.isError && <AuthFormError errorMessage={formState.errorMessage} />}
+      <button
+        type="submit"
+        disabled={isPending}
+        className="text-bold min-h-14 w-full rounded-full bg-gray-200 p-4 text-center text-gray-900 transition-colors hover:bg-gray-300 disabled:cursor-not-allowed disabled:bg-gray-300 disabled:text-gray-400 dark:bg-gray-600 dark:text-gray-50 hover:dark:bg-gray-500 disabled:dark:bg-gray-700"
+      >
+        {isPending ? (
+          <Spinner
+            width={20}
+            height={20}
+            className="inline animate-spin fill-gray-500 text-center text-gray-50 dark:fill-gray-50 dark:text-gray-600"
+          />
+        ) : type === 'login' ? (
+          'Log In'
+        ) : (
+          'Sign Up'
+        )}
+      </button>
     </form>
   );
-}
-
-export function LoginForm({ onSubmit }: { onSubmit: AuthFormProps['onSubmit'] }) {
-  return <AuthForm type="login" onSubmit={onSubmit} />;
-}
-
-export function SignupForm({ onSubmit }: { onSubmit: AuthFormProps['onSubmit'] }) {
-  return <AuthForm type="signup" onSubmit={onSubmit} />;
 }
